6a9c5639a1e129e3e28abd1513904e4eb8324206
Zagroww <81807989+Zagroww@users.noreply.github.com>
2025-04-15 18:17:18 +0200

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -1,23 +1,19 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
 const cloudinary = require('cloudinary').v2;
+const path = require('path');
 
+// Configurazione di Cloudinary usando variabili d'ambiente
 cloudinary.config({
   cloud_name: process.env.dzs4f4erb,
   api_key: process.env.666349749824897,
   api_secret: process.env.T47bQbChrnaXW4IeYkvnYlC5qnQ,
 });
 
-// Carica l'immagine nella cartella 'prova'
-// src/utils/cloudinary.js
-
+// Funzione per caricare l'immagine su Cloudinary
 const uploadImage = async (filePath) => {
   try {
-    console.log(`Caricamento dell'immagine da: ${filePath}`); // Log per verificare il percorso
+    console.log(`Caricamento dell'immagine da: ${filePath}`); // Log per verificare il percorso del file
     const result = await cloudinary.uploader.upload(filePath, {
-      folder: 'prova', // Cartella su Cloudinary
+      folder: 'prova', // Cartella su Cloudinary in cui salvare l'immagine
     });
 
     console.log('Immagine caricata con successo! URL:', result.secure_url); // Log dell'URL restituito da Cloudinary
@@ -28,6 +24,4 @@ const uploadImage = async (filePath) => {
   }
 };
 
-module.exports = {
-  uploadImage,
-};
+module.exports = { uploadImage };
