44ee3a1165868958b34a4064eb53e99a90584ffc
Zagroww <81807989+Zagroww@users.noreply.github.com>
2025-04-15 17:40:09 +0200

    Add files via upload

---
+++ cloudinary.js
@@ -0,0 +1,10 @@
+// cloudinary.js
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: 'dzs4f4erb',
+  api_key: '666349749824897',
+  api_secret: 'T47bQbChrnaXW4IeYkvnYlC5qnQ'
+});
+
+module.exports = cloudinary;
